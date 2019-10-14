d12d27566bc18e97ff726ff65445094662640849
idosinchuk <idosinchuk@ITEM-S84804.emea.msad.sopra>
2019-10-14 13:49:16 +0200

    Changed application

--- src/main/java/com/idosinchuk/teagile/chat/service/ChatApplication.java
+++ src/main/java/com/idosinchuk/teagile/chat/service/ChatApplication.java
@@ -2,7 +2,9 @@ package com.idosinchuk.teagile.chat.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ChatApplication {
 
--- src/test/java/com/idosinchuk/teagile/chat/service/ChatApplicationTests.java
+++ src/test/java/com/idosinchuk/teagile/chat/service/ChatApplicationTests.java
@@ -1,4 +1,4 @@
-package com.idosinchuk.teagile.communication.service;
+package com.idosinchuk.teagile.chat.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CommunicationApplicationTests {
+public class ChatApplicationTests {
 
 	@Test
 	public void contextLoads() {
